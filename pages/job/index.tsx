c4e24731d81fe90f84daf270231f8d8b99855c58
Mouhealdien <mhyaldynalamyn45@gmail.com>
2023-08-19 23:04:15 +0300

    aaa

--- pages/job/index.tsx
+++ pages/job/index.tsx
@@ -3,24 +3,131 @@ import CompaniesHome from '../../components/CompaniesHome'
 import { useState,useEffect } from 'react';
 import HeroSearchInput from '../../components/HeroSearchInput'
 import axios from 'axios';
+import JobsCardHome from '../../components/JobsCardHome';
 import { responseParser } from '../../lib/helper';
 const index = () => {
+  const data = [
+    {
+      title: "Software developer",
+      image: '',
+      code: "5887",
+      date: "12",
+      role: "IT",
+      level: "junior",
+      type: "part time",
+      experience: "2",
+      location: "Aleppo",
+      company: "source code",
+      link: "/",
+    },
+    {
+      title: "Software developer",
+      image: '',
+      code: "5887",
+      date: "12",
+      role: "IT",
+      level: "junior",
+      type: "part time",
+      experience: "2",
+      location: "Aleppo",
+      company: "source code",
+      link: "/",
+    },
+    {
+      title: "Software developer",
+      image: '',
+      code: "5887",
+      date: "12",
+      role: "IT",
+      level: "junior",
+      type: "part time",
+      experience: "2",
+      location: "Aleppo",
+      company: "source code",
+      link: "/",
+    },
+    {
+      title: "Software developer",
+      image: '',
+      code: "5887",
+      date: "12",
+      role: "IT",
+      level: "junior",
+      type: "part time",
+      experience: "2",
+      location: "Aleppo",
+      company: "source code",
+      link: "/",
+    },
+    {
+      title: "Software developer",
+      image: '',
+      code: "5887",
+      date: "12",
+      role: "IT",
+      level: "junior",
+      type: "part time",
+      experience: "2",
+      location: "Aleppo",
+      company: "source code",
+      link: "/",
+    },
+    {
+      title: "Software developer",
+      image: '',
+      code: "5887",
+      date: "12",
+      role: "IT",
+      level: "junior",
+      type: "part time",
+      experience: "2",
+      location: "Aleppo",
+      company: "source code",
+      link: "/",
+    },
+    {
+      title: "Software developer",
+      image: '',
+      code: "5887",
+      date: "12",
+      role: "IT",
+      level: "junior",
+      type: "part time",
+      experience: "2",
+      location: "Aleppo",
+      company: "source code",
+      link: "/",
+    },
+    {
+      title: "Software developer",
+      image: '',
+      code: "5887",
+      date: "12",
+      role: "IT",
+      level: "junior",
+      type: "part time",
+      experience: "2",
+      location: "Aleppo",
+      company: "source code",
+      link: "/",
+    },
+  ];
+  
   const [search, setsearch] = useState();
   const handleSearch = (e) => {
     setsearch(e.target.value);
     console.log(search);
   };
-  const [Companies, setCompanies] = useState([]);
+  const [jobs, setjobs] = useState([]);
 
   useEffect(() => {
     const fetchDataAsync = async () => {
 
       try {
-        const { data: response } = await axios(`/companies?populate[0]=profileImg`);
+        const { data: response } = await axios(`/jobs`);
 
 
-
-        setCompanies(responseParser(response) as any)
+        setjobs(responseParser(response) as any)
 
       } catch (error) {
 
@@ -30,7 +137,7 @@ const index = () => {
 
     fetchDataAsync();
   }, []);
-  console.log(Companies && Companies[0])
+  console.log(jobs)
   return (
     <div className=' felx flex-col justify-center items-center'>
       <div className=' px-28'>
@@ -38,6 +145,12 @@ const index = () => {
       </div>
     
       <CompaniesHome/>
+
+      <div className=' flex flex-row items-center justify-center'> 
+          {
+            
+          }
+      </div>
     </div>
   )
 }
