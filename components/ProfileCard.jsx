cd90a9250697bd1cc7b37411f15011915e930a3a
Mouhealdien <mhyaldynalamyn45@gmail.com>
2023-08-20 04:24:28 +0300

    profile card edit links

--- components/ProfileCard.jsx
+++ components/ProfileCard.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Image from 'next/image'
 import male from '../assets/boy.png'
 import female from '../assets/boy.png'
-const ProfileCard = ({ name, gender, Address, educationlevel, yearsOFExperience, age, img }) => {
+import Link from 'next/link'
+const ProfileCard = ({id, name, gender, Address, educationlevel, yearsOFExperience, age, img }) => {
   return (
     <div>
+      <Link href={`/profile/${id}`}>
       <div className=' border-b-2 border-b-gray-300 flex flex-row gap-2 items-center max-w-md border-gray-300 border-[3px] p-4 border-l-primary  border-l-[6px]  shadow-xl  rounded-md'>
-        <Image width={300} height={300} className='w-32 h-32 rounded-full' src={img ? img : gender == "Male" ? male : female} />
+        <img width={300} height={300} className='w-32 h-32 rounded-full' src={img ? img : gender == "Male" ? male : female} />
         <div className='flex flex-col'>
 
           <ul className=''>
@@ -18,6 +20,7 @@ const ProfileCard = ({ name, gender, Address, educationlevel, yearsOFExperience,
           </ul>
         </div>
       </div>
+      </Link>
     </div>
   )
 }
