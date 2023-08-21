cd66dc7e8de8239420843df29e26a916d1032fb1
Mouhealdien <mhyaldynalamyn45@gmail.com>
2023-08-21 14:41:26 +0300

    edit styles

--- components/JobDetailsCard.jsx
+++ components/JobDetailsCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const JobDetailsCard = ({id,gender,jobRole,age,carerrLevel,education,jobType,experience,salary}) => {
   return (
     <div>
-    <div className=' py-4  border-gray-300 border-[3px]  shadow-2xl  rounded-md border-l-primary   border-l-[6px] max-w-lg bg-secondary items-center '>
+    <div className=' w-full py-4  border-gray-300 border-[3px]  shadow-2xl  rounded-md border-l-primary   border-l-[6px] max-w-lg bg-secondary items-center '>
         <h2 className='text-primary px-3 text-xl  font-bold border-gray-300 border-b-2 pb-2 '>Job Details</h2>
         <div className='flex flex-col gap-5 p-5 m-5  '>
             <div className=' py-4 border-b-2 border-gray-300 flex flex-row gap-5 px-3'>
